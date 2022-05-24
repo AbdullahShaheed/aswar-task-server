eb809a1311097d9fb2eab7c116764efbe4640d2e
Abd <abdullahzwed@gmail.com>
2022-05-24 22:24:20 +0300

    fix the problem of CORS via a middleware

--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,12 @@ const logins = require("../routes/logins");
 
 module.exports = function (app) {
   app.use(express.json());
+  app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    next();
+  });
   app.use("/api/products", products);
   app.use("/api/users", users);
   app.use("/api/logins", logins);
